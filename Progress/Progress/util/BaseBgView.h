e3aba15827885af6971b8fc690e6938667fd15a4
龙哥 <songfeilong@fdjf.local>
2016-01-26 14:30:29 +0800

    12323

---
+++ Progress/Progress/util/BaseBgView.h
@@ -0,0 +1,17 @@
+//
+//  BaseBgView.h
+//  Progress
+//
+//  Created by 宋飞龙 on 16/1/26.
+//  Copyright © 2016年 Mr Song. All rights reserved.
+//
+
+#import "BaseProgressView.h"
+
+@interface BaseBgView : BaseProgressView
+- (void)show;
+
+- (void)hide;
+
+- (void)buildView;
+@end
---
+++ Progress/Progress/util/BaseBgView.m
@@ -0,0 +1,32 @@
+//
+//  BaseBgView.m
+//  Progress
+//
+//  Created by 宋飞龙 on 16/1/26.
+//  Copyright © 2016年 Mr Song. All rights reserved.
+//
+
+#import "BaseBgView.h"
+
+@implementation BaseBgView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (void)show {
+    
+}
+
+- (void)hide {
+    
+}
+
+- (void)buildView {
+    
+}
+
+@end
